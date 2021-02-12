a621f7c1b90290cd5eab3f0692b15b5ecc78ca15
Thomas Frazier <tfrazier2424@gmail.com>
2021-02-12 10:52:26 -0500

    added a delete note button

--- src/scripts/notes/noteDataProvider.js
+++ src/scripts/notes/noteDataProvider.js
@@ -21,3 +21,9 @@ export const saveNote = note => {
     })
     // .then(getNotes) // After we add a note, get them all again so our new note appears in our collection
 }
+
+export const deleteNote = noteId => {
+    return fetch(`http://localhost:8088/notes/${noteId}`, {
+        method: "DELETE"
+    })
+}
--- src/scripts/notes/noteList.js
+++ src/scripts/notes/noteList.js
@@ -1,6 +1,7 @@
 import {getNotes, useNotes} from "./noteDataProvider.js"
 import { Note } from "./note.js"
 import { useCriminals, getCriminals } from "../criminals/criminalDataProvider.js"
+import {deleteNote} from './noteDataProvider.js'
 
 // This may be a slight refactor to what you have in your code. We added a render function to print our criminals to the DOM. You can use the render function as we do in this example, or you can keep your logic in your NoteList function-- it's up to you.
 
@@ -15,6 +16,7 @@ const render = (noteCollection, criminalCollection) => {
             <section class="note">
                 <h2>Note about ${relatedCriminal.name}</h2>
                 ${note.text}
+                <button id="deleteNote--${note.id}">Delete</button>
             </section>
         `
     }).join("")
@@ -29,3 +31,17 @@ export function NoteList(){
         render(notes, criminals)
     })
 }
+
+const eventHub = document.querySelector("main")
+
+eventHub.addEventListener("click", (eventObject) => {
+  if (eventObject.target.id.startsWith("deleteNote--")) {
+    const idToDelete = eventObject.target.id.split("-")[2]
+    // ---------- Write your code here -------------//
+    // Call the deleteNote function and pass in the appropriate id
+    // console.log("you clicked the delete button")
+    deleteNote(idToDelete)
+    // Then call NoteList to refresh the list of notes
+    .then(NoteList)
+  }
+});
